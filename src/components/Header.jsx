d4f82b62b55532313bda8fb5824dd6190ecd5304
Rob Sassack <rsassack25@gmail.com>
2022-11-03 03:30:55 -0400

    Added header component with styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div className='header--logo-container'>
+        <span className='header--logo'>😂</span>
+        <span className='header--title'>Meme Generator</span>
+      </div>
+      <div className='header--desc'>
+        <span>React Course - Project 3</span>
+      </div>
+    </header>
+  );
+}
