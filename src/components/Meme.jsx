d1bccdb5c0bd6bc8629d33dbb5bb8164642c438f
Rob Sassack <rsassack25@gmail.com>
2022-11-04 22:45:12 -0400

    Added random image selection

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,7 +1,20 @@
+import React from "react";
+import memesData from "../memesData";
+
 export default function Meme() {
+  const [memeUrl, setMemeUrl] = React.useState('');
+  const [memeAlt, setMemeAlt] = React.useState('');
+
+  function getMeme() {
+    let memeArr = memesData.data.memes;
+    let randomMeme = memeArr[Math.floor(Math.random() * memeArr.length)];
+    setMemeUrl(randomMeme.url);
+    setMemeAlt(`${randomMeme.name} meme`);
+  }
+
   return (
     <div className='meme'>
-      <form action='post' className="meme--form">
+      <div className='meme--form'>
         <input
           type='text'
           name='topText'
@@ -18,8 +31,10 @@ export default function Meme() {
           type='button'
           value='Get a new meme image 🖼'
           className='meme--submit'
+          onClick={getMeme}
         />
-      </form>
+      </div>
+      <img src={memeUrl} alt={memeAlt} className='meme--image' />
     </div>
   );
 }
